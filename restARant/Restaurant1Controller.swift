db11870865ac5933b560850b608041ed19ee7027
Patrick Maribojoc <pmaribojoc@captechventures.com>
2018-05-07 19:23:30 -0400

    Added fruit cake asset

--- restARant/Restaurant1Controller.swift
+++ restARant/Restaurant1Controller.swift
@@ -200,6 +200,31 @@ class Restaurant1Controller: UIViewController, ARSCNViewDelegate {
         
     }
     
+    @IBAction func addFruitCake(_ sender: UIButton) {
+        guard let node = globalNode else{
+            return
+        }
+        for childNode in node.childNodes {
+            childNode.removeFromParentNode()
+        }
+        
+        guard let breadScene = SCNScene(named: "art.scnassets/fruit-cake/fruit-cake.dae") else { return }
+        let breadNode = SCNNode()
+        let breadSceneChildNodes = breadScene.rootNode.childNodes
+        
+        for childNode in breadSceneChildNodes {
+            breadNode.addChildNode(childNode)
+        }
+        
+        let x = CGFloat(globalAnchor.center.x)
+        let y = CGFloat(globalAnchor.center.y)
+        let z = CGFloat(globalAnchor.center.z)
+        breadNode.position = SCNVector3(x,y,z)
+        breadNode.scale = SCNVector3(0.25, 0.25, 0.25)
+        node.addChildNode(breadNode)
+        
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 //
